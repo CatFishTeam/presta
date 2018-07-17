44b1e7d1542f0b6beee45944a94f25dd8d2dbda1
Mayon Maël <mael.mayon@free.fr>
2018-07-17 16:44:47 +0200

    Boom :fire:

--- themes/classic/_dev/js/navbar.js
+++ themes/classic/_dev/js/navbar.js
@@ -1,8 +1,13 @@
 import $ from 'jquery';
 
 $(document).ready(function () {
+    /*
     $('#category-11 a').attr('href', '#')
     $('#category-11').on('click', function(){
         $(this).children('div').toggle();
+    })
+    */
+    $('.dropdown-item').on('click', function() {
+        $(this).next('div').toggle();
     })
 });
