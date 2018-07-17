c983f2bf60f8106825e88357abd9c02a3a45cea3
Robilol <pimous78920@gmail.com>
2018-07-17 18:07:25 +0200

    hover menu

--- themes/classic/_dev/js/navbar.js
+++ themes/classic/_dev/js/navbar.js
@@ -2,9 +2,15 @@ import $ from 'jquery';
 
 $(document).ready(function () {
     $('.dropdown-item').next('div').hide()
-    $('.dropdown-item[data-depth="0"], #category-12 > a').attr('href', '#')
+    $('.dropdown-item[data-depth="0"]').attr('href', '#')
     $('.dropdown-item').on('click', function() {
         $(this).next('div').toggle();
     })
-
+    $("#category-12 a").hover(
+        function() {
+            $( this ).next('div').show();
+        }, function() {
+            $( this ).next('div').hide();
+        }
+    );
 });
