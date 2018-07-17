77137231ac44826d51b2cbd528561cf5c45d33d1
Baldrani <mael.mayon@free.fr>
2018-07-17 17:23:01 +0200

    Update navbar.js

--- themes/classic/_dev/js/navbar.js
+++ themes/classic/_dev/js/navbar.js
@@ -1,14 +1,8 @@
 import $ from 'jquery';
 
 $(document).ready(function () {
-    /*
-    $('#category-11 a').attr('href', '#')
-    $('#category-11').on('click', function(){
-        $(this).children('div').toggle();
-    })
-    */
     $('.dropdown-item').next('div').hide()
-    $('.dropdown-item').attr('href', '#')
+    $('.dropdown-item[data-depth="0"], #category-12 > a').attr('href', '#')
     $('.dropdown-item').on('click', function() {
         $(this).next('div').toggle();
     })
